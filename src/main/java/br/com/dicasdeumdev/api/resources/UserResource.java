f0f0acf5e8ba5b972a67aef821e2df227ef6fd0b
emygdioBarreto <emygdio.barreto@gmail.com>
2025-02-07 07:24:56 -0300

    Classe UserResource findById

--- src/main/java/br/com/dicasdeumdev/api/resources/UserResource.java
+++ src/main/java/br/com/dicasdeumdev/api/resources/UserResource.java
@@ -1,6 +1,7 @@
 package br.com.dicasdeumdev.api.resources;
 
 import br.com.dicasdeumdev.api.domain.User;
+import br.com.dicasdeumdev.api.services.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResource {
 
+    private final UserService service;
+
+    public UserResource(UserService service) {
+        this.service = service;
+    }
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok().body(new User(1,"emygdio2", "teste@teste.com", "1234"));
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }
