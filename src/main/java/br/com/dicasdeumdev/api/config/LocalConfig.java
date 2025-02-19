ad5b0626afc95f17e5584e6ae5215e6bd7b4b6e0
emygdioBarreto <emygdio.barreto@gmail.com>
2025-02-19 09:21:30 -0300

    Ajuste na classe LocalConfig para evitar repetitivas gravações na inicialização

--- src/main/java/br/com/dicasdeumdev/api/config/LocalConfig.java
+++ src/main/java/br/com/dicasdeumdev/api/config/LocalConfig.java
@@ -2,6 +2,7 @@ package br.com.dicasdeumdev.api.config;
 
 import br.com.dicasdeumdev.api.domain.User;
 import br.com.dicasdeumdev.api.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -14,6 +15,7 @@ public class LocalConfig {
 
     private final UserRepository repository;
 
+    @Autowired
     public LocalConfig(UserRepository repository) {
         this.repository = repository;
     }
@@ -23,6 +25,6 @@ public class LocalConfig {
         User u1 = new User(null, "Emygdio", "emygdio.barreto@gmail.com", "paladinoT8");
         User u2 = new User(null, "Flavia", "flavia.coelho@gmail.com", "popa!2111");
 
-        return repository.saveAll(List.of(u1,u2));
+        return null; //repository.saveAll(List.of(u1,u2));
     }
 }
