0c38a0e9d55477484d13cd1c1ae27e9b0bc6d238
emygdioBarreto <emygdio.barreto@gmail.com>
2025-02-19 09:56:17 -0300

    adicionado classe de tratamento de excessao do resource de testes

--- src/main/java/br/com/dicasdeumdev/api/resources/exceptions/StandardError.java
+++ src/main/java/br/com/dicasdeumdev/api/resources/exceptions/StandardError.java
@@ -20,31 +20,15 @@ public class StandardError {
         return timestamp;
     }
 
-    public void setTimestamp(LocalDateTime timestamp) {
-        this.timestamp = timestamp;
-    }
-
     public Integer getStatus() {
         return status;
-    }
-
-    public void setStatus(Integer status) {
-        this.status = status;
     }
 
     public String getError() {
         return error;
     }
 
-    public void setError(String error) {
-        this.error = error;
-    }
-
     public String getPath() {
         return path;
-    }
-
-    public void setPath(String path) {
-        this.path = path;
     }
 }
