5799457443e29685ab678b985e3371891b85467e
emygdioBarreto <emygdio.barreto@gmail.com>
2025-02-19 09:57:18 -0300

    ajuste na classe de resource User

--- src/main/java/br/com/dicasdeumdev/api/resources/UserResource.java
+++ src/main/java/br/com/dicasdeumdev/api/resources/UserResource.java
@@ -1,64 +1,57 @@
 package br.com.dicasdeumdev.api.resources;
 
 import br.com.dicasdeumdev.api.domain.dto.UserDTO;
-import br.com.dicasdeumdev.api.repositories.UserRepository;
 import br.com.dicasdeumdev.api.services.UserService;
-import br.com.dicasdeumdev.api.services.exceptions.DataIntegratyViolationException;
 import org.modelmapper.ModelMapper;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
 
+    private static final String ID = "/{id}";
+
     private final UserService service;
     private final ModelMapper mapper;
-    private final UserRepository repository;
 
-    public UserResource(UserService service, ModelMapper mapper, UserRepository repository) {
+    public UserResource(UserService service, ModelMapper mapper) {
         this.service = service;
         this.mapper = mapper;
-        this.repository = repository;
     }
 
-    @GetMapping(value = "/{id}")
+    @GetMapping(value = ID)
     public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
         return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
     }
 
     @GetMapping
     public ResponseEntity<List<UserDTO>> findAll() {
-        return ResponseEntity.ok()
-                .body(service.findAll()
-                        .stream()
-                        .map(user -> mapper.map(user, UserDTO.class)).toList());
+        return ResponseEntity.ok().body(service.findAll()
+                .stream().map(user -> mapper.map(user, UserDTO.class)).toList());
     }
 
     @PostMapping
     public ResponseEntity<UserDTO> create(@RequestBody UserDTO obj) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(mapper.map(service.create(obj), UserDTO.class));
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest().path(ID).buildAndExpand(service.create(obj).getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 
-    @PutMapping("/{id}")
+    @PutMapping(value = ID)
     public ResponseEntity<UserDTO> update(@PathVariable Integer id, @RequestBody UserDTO obj) {
-        return repository.findById(id)
-                .map(user -> {
-                    user.setName(obj.getName());
-                    if (id.equals(obj.getId()) && !user.getEmail().equals(obj.getEmail())) {
-                        user.setEmail(obj.getEmail());
-                    }
-                    obj.setPassword(user.getPassword());
-                    return ResponseEntity.ok().body(mapper.map(service.update(obj), UserDTO.class));
-                }).orElseThrow(() -> new DataIntegratyViolationException("Email já cadastrado no sistema"));
+        obj.setId(id);
+        return ResponseEntity.ok().body(mapper.map(service.create(obj), UserDTO.class));
     }
 
-    @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable Integer id) {
+    @DeleteMapping(value = ID)
+    public ResponseEntity<UserDTO> delete(@PathVariable Integer id) {
         service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
